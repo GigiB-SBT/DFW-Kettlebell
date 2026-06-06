4699526a2eec9af0d6cf030307a8a164d45fe221
GigiB-SBT <bl23bsns@gmail.com>
2026-06-06 16:17:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE="dfw-v2";
-const URLS=["./","./index.html","./manifest.json","./icon.svg"];
+const CACHE="dfw-v3";
+const URLS=[
+  "./index.html",
+  "./manifest.json",
+  "./icon.svg",
+  "./sw.js"
+];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)));
   self.skipWaiting();
@@ -8,7 +13,10 @@ self.addEventListener("activate",e=>{
   e.waitUntil(caches.keys().then(ks=>Promise.all(
     ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))
   )));
+  self.clients.claim();
 });
 self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html")))
+  );
 });
